61212f3a11a42973c0870ea2e35845dc92768071
l3l4isesod <genielogicielisesod@gmail.com>
2023-06-29 16:29:12 +0200

    Salutation

--- TestTravailCollaboratif/TestTravailCollaboratif/Form1.cs
+++ TestTravailCollaboratif/TestTravailCollaboratif/Form1.cs
@@ -19,7 +19,7 @@ namespace TestTravailCollaboratif
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Bonjour");
+            MessageBox.Show("Bonjour L3 L4 ISESOD");
         }
     }
 }
