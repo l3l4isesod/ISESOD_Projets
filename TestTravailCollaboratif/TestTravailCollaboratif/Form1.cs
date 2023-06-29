12979330a3afb32ae5d59592fe0a7bcfbb1ba2bc
l3l4isesod <genielogicielisesod@gmail.com>
2023-06-29 16:22:53 +0200

    Ajout evenement

--- TestTravailCollaboratif/TestTravailCollaboratif/Form1.cs
+++ TestTravailCollaboratif/TestTravailCollaboratif/Form1.cs
@@ -16,5 +16,10 @@ namespace TestTravailCollaboratif
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bonjour");
+        }
     }
 }
